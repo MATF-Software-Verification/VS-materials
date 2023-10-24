b5b5a69b3bd9426ef210e765804a69d571b9dfee
ivan-ristovic <work@ristovic.net>
2023-10-24 23:04:43 +0200

    Add Java native demo; move old native to aot

--- 01_debugging/03_java/02_native/HelloWorld.java
+++ 01_debugging/03_java/02_native/HelloWorld.java
@@ -1,14 +1,12 @@
-import java.util.*;
+class HelloWorld {
+    static {
+        System.loadLibrary("HelloWorld");
+    }
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("----- Hello, world! -----");
-        System.out.println("args: " + Arrays.toString(args));
-        System.out.println(System.getProperty("java.vm.name"));
-        System.out.println(System.getProperty("java.home"));
-        System.out.println(System.getProperty("java.vendor"));
-        System.out.println(System.getProperty("java.version"));
-        System.out.println(System.getProperty("java.specification.vendor"));
-        System.out.println("-------------------------");
+    private native void print();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(10000);
+        new HelloWorld().print();
     }
 }
