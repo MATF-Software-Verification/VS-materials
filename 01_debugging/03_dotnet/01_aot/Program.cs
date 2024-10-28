28ad3479672b4d670aef5c5b3c07fa47b0eeaa01
Ivan Ristovic <work@ristovic.net>
2024-10-28 16:23:53 +0100

    Add record example for CIL viewing

--- 01_debugging/03_dotnet/01_aot/Program.cs
+++ 01_debugging/03_dotnet/01_aot/Program.cs
@@ -1,1 +1,8 @@
 ﻿Console.WriteLine("Hello, World!");
+new MyRecord("Hello", 5).MyRecordMethod();
+
+public record MyRecord(string S, int V)
+{
+    public string MyRecordMethod() => $"{this.S}: {this.V}";
+}
+
